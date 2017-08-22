7a45f9f1ce1eb01e0cb559b84954e45d603a75f8
Marek Sebera <marek.sebera@gmail.com>
2017-08-22 19:41:17 +0200

    rollback

--- fix_csv_mysql.sh
+++ fix_csv_mysql.sh
@@ -3,5 +3,6 @@
 find CSV -type f -iname '*.csv' -exec sed -i 's/,false,/,0,/g' \{\} \;
 find CSV -type f -iname '*.csv' -exec sed -i 's/,true,/,1,/g' \{\} \;
 
-find CSV -type f -iname '*.csv' -exec sed -i 's/000Z.$/000/g' \{\} \;
-find CSV -type f -iname '*.csv' -exec sed -i 's/000Z,/000,/g' \{\} \;
+# does not get rid of warnings
+#find CSV -type f -iname '*.csv' -exec sed -i 's/000Z.$/000/g' \{\} \;
+#find CSV -type f -iname '*.csv' -exec sed -i 's/000Z,/000,/g' \{\} \;
