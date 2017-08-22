c9f7c54be11798ed331f483b46853529f97c4e7f
Marek Sebera <marek.sebera@gmail.com>
2017-08-22 19:03:30 +0200

    utilities LoC and csv mass-sed fixup

---
+++ fix_csv_mysql.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+find CSV -type f -iname '*.csv' -exec sed -i 's/,false,/,0,/g' \{\} \;
+find CSV -type f -iname '*.csv' -exec sed -i 's/,true,/,1,/g' \{\} \;
+
+find CSV -type f -iname '*.csv' -exec sed -i 's/000Z.$/000/g' \{\} \;
+find CSV -type f -iname '*.csv' -exec sed -i 's/000Z,/000,/g' \{\} \;
